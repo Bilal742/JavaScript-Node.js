cfcc8b09e3a46ec82a8f6682fcaaa21d7f0c6e3e
Bilal Usman <bilalusman1291@gmail.com>
2026-01-05 13:18:09 +0500

    🚀 Node.Js Modules with Examples

--- Backend Tutorial Node.Js Modules/app.js
+++ Backend Tutorial Node.Js Modules/app.js
@@ -1,28 +1,24 @@
-// const { createServer } = require("http");
-
-// const port = 3000;
-// const hostname = "127.0.0.1"
-
-// const server = createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader("content-Type", "text/html");
-//     res.end("<h1>Heloo</h1>");
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log(`server in running: http://${hostname}:${port}/`);
-// });
-
-
+const { createServer } = require("http");
 const fs = require("fs");
+
+const port = 3000;
+const hostname = "127.0.0.1";
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("content-Type", "text/html");
+    res.end("<h1>hyee</h1>");
+});
+
+server.listen(port, hostname, () => {
+    console.log(`server on running: http://${hostname}:${port}/`);
+});
+
 const text = fs.readFileSync("index.html", "utf-8");
 console.log(text);
 
- 
+fs.writeFileSync("test.txt", "Hello Node.js");
+const text1 = fs.readFileSync("test.txt", "utf-8");
+console.log(text1);
 
 
-// const fs = require("fs");
-// fs.writeFileSync("test.txt", "Hello Node.js");
-// const text1 = fs.readFileSync("test.txt", "utf-8");
-
-// console.log(text1);
