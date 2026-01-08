40fbec957bec7452223ccf6375726845f8157fd6
Bilal Usman <bilalusman1291@gmail.com>
2026-01-08 13:01:48 +0500

    🎨 UI improvement

--- Backend Tutorial Node.Js Modules/app.js
+++ Backend Tutorial Node.Js Modules/app.js
@@ -1,24 +1,23 @@
-const { createServer } = require("http");
-const fs = require("fs");
+const { createServer } = require("node:http");
+const fs = require("fs")
 
 const port = 3000;
-const hostname = "127.0.0.1";
+const hostname = "127.0.0.1"
 
 const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("content-Type", "text/html");
-    res.end("<h1>hyee</h1>");
+    res.statusCode = 200,
+        res.setHeader("content-Type", "text/html"),
+        res.end("<h1>Hello</h1>")
 });
 
 server.listen(port, hostname, () => {
-    console.log(`server on running: http://${hostname}:${port}/`);
-});
-
-const text = fs.readFileSync("index.html", "utf-8");
-console.log(text);
-
-fs.writeFileSync("test.txt", "Hello Node.js");
-const text1 = fs.readFileSync("test.txt", "utf-8");
-console.log(text1);
+    console.log(`server in running: http://${hostname}:${port}/`);
+}); 
 
 
+let text = fs.readFileSync("test.txt", "utf-8");
+text = text.replace("Node", "Java")
+fs.writeFileSync("style.txt", text); 
+
+
+console.log(text); 
