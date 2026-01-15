612d564d4b33bb8538054f9d5938985ea8e1b3ae
Bilal Usman <bilalusman1291@gmail.com>
2026-01-15 17:40:38 +0500

    🚀 Node.js Introduction and Installation setup

--- Backend_tutorial_node.Js_Introduction_and_Installation/app.js
+++ Backend_tutorial_node.Js_Introduction_and_Installation/app.js
@@ -1,25 +1,29 @@
-const { createServer } = require('node:http');
+// const { createServer } = require('node:http');
 
-const hostname = "127.0.0.1"
-const port = "3000"
-
-const server = createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader("content-Type", "text/html");
-  res.end("<h1>Hello World</h1>");
-});
-
-server.listen(port, hostname, () => {
-  console.log(`server in running: http://${hostname}:${port}/`);
- 
-});
-
-
-// const { createServer } = require("http");
+// const hostname = "127.0.0.1"
+// const port = "3000"
 
 // const server = createServer((req, res) => {
-//   res.write("Hello World");
-//   res.end();
+//   res.statusCode = 200
+//   res.setHeader("content-Type", "text/html");
+//   res.end("<h1>Hello World</h1>");
 // });
 
-// server.listen(3000);
+// server.listen(port, hostname, () => {
+//   console.log(`server in running: http://${hostname}:${port}/`);
+
+// });
+
+
+
+const { createServer } = require("http");
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("content-Type", "text/html");
+  res.end("<h1>Hello</h1>")
+});
+
+server.listen(3000, "127.0.0.1", () => {
+  console.log("server on running: http://127.0.0.1:3000/");
+});
