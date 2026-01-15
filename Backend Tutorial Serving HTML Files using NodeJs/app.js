c439dc722c98417d59aaa8b2d03e38ce957a53e9
Bilal Usman <bilalusman1291@gmail.com>
2026-01-15 17:38:12 +0500

    ?? Serving HTML Files using NodeJs with Examples

--- Backend Tutorial Serving HTML Files using NodeJs/app.js
+++ Backend Tutorial Serving HTML Files using NodeJs/app.js
@@ -2,12 +2,11 @@ const http = require("http");
 const fs = require("fs");
 const fileRead = fs.readFileSync("text.html", "utf-8");
 
-
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/html" });
     res.end(fileRead);
 });
 
 server.listen(8000, "127.0.0.1", () => {
-    console.log("listening on port 80")
+    console.log("listening on port 80");
 });
