47298a1f28234ebdab833f341611de6458e04036
Bilal Usman <bilalusman1291@gmail.com>
2026-01-20 01:13:29 +0500

    ?? updates files

--- Creating a Custom Backend Using NodeJs/app.js
+++ Creating a Custom Backend Using NodeJs/app.js
@@ -1,39 +1,40 @@
-const http = require("http");
+const { createServer } = require("http");
 const fs = require("fs");
 
-const readFile = fs.readFileSync("test.txt", "utf-8");
 
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    let url = req.url;
+const server = createServer((req, res) => {
     const home = fs.readFileSync("home.html");
     const about = fs.readFileSync("about.html");
     const services = fs.readFileSync("services.html");
     const contact = fs.readFileSync("contact.html");
-
     res.writeHead(200, "content-Type", "text/html");
+
+    let url = req.url;
+
     if (url === "/") {
         res.end(home);
     }
 
     else if (url === "/about") {
-        res.end(about);
+        res.end(about)
     }
 
     else if (url === "/services") {
-        res.end(services);
+        res.end(services)
     }
 
     else if (url === "/contact") {
-        res.end(contact);
+        res.end(contact)
     }
 
     else {
-        res.statusCode = 404;
-        res.end("<h1>404 not found");
-    }
+        res.statusCode = 200;
+        "<h1>404 not found</h1>"
+    };
+
 });
 
 server.listen(3000, "127.0.0.1", () => {
-    console.log(`server in running: http://127.0.0.1:3000/`);
-}); 
+    console.log("server in running: http://127.0.0.1:3000");
+
+})
