e72978666b276fb41156f8b9531d25da957c471f
Bilal Usman <bilalusman1291@gmail.com>
2026-01-20 23:37:07 +0500

    set up Creating Custom Modules in Node Using NodeJs

---
+++ Creating Custom Modules in Node Using NodeJs/index.js
@@ -0,0 +1,5 @@
+const mod = require("./mod");
+
+// console.log(mod.avg([3, 4, 5]));
+console.log(mod.name, mod.age);
+
---
+++ Creating Custom Modules in Node Using NodeJs/mod.js
@@ -0,0 +1,17 @@
+console.log("Hello world");
+
+function average(arr) {
+    let sum = 0;
+    arr.forEach(element => {
+        sum += element;
+    });
+    return sum + arr.length;
+};
+
+module.exports = {
+    avg: average,
+    name: "bilal",
+    age: 18
+};
+
+module.exports.name = "ali"
